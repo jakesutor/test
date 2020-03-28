36790429ca8a6450b0fb017e7f24c3cddbac1892
jakesutor <jake.sutor@gmail.com>
2020-03-28 11:15:08 -0400

    final project

---
+++ CS504 Final Project.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 21:20:05 2020
+
+@author: jakes
+"""
+
+import pandas as pd
+
+stats = pd.read_csv(r'C:\Users\jakes\Documents\DAEN\Spring 2020\CS504\Season_Stats.csv')
+
+
+
+
+
